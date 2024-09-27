11e5f2dd6b609b37bd1faf6b3fd611d5437f011b
sindhu718 <vagasindhu718@gmail.com>
2024-09-27 16:26:17 -0400

    Code Change

--- calculator.go
+++ calculator.go
@@ -2,11 +2,9 @@ package main
 
 import "fmt"
 
-// calculator function to perform basic arithmetic operations
 func calculator(property string, a, b int) int {
     var x int
 
-    // Perform the appropriate calculation based on the property value
     switch property {
     case "add":
         x = a + b
@@ -17,17 +15,13 @@ func calculator(property string, a, b int) int {
     case "division":
         if b != 0 {
             x = a / b
-        } else {
-            fmt.Println("Error: Division by zero")
         }
 	case "modulus":
         if b != 0 {
             x = a % b
-        } else {
-            fmt.Println("Error: Division by zero in modulus operation")
         }
     default:
-        fmt.Println("Unsupported operation")
+        fmt.Println("undefined")
     }
 
     return x
