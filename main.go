269f615d319e889cc27ca73bde1a6f94dba3bd03
avneetpandey82 <avneetpandey82@gmail.com>
2024-09-29 18:17:53 -0400

    merge issue resolved/scanner added to take the input from user

--- main.go
+++ main.go
@@ -3,10 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	a, b := swap("loyalist ", "college ")
-	print(a, b)
 	fmt.Println("Welcome to Group D's Week 4 Project!")
+
+	//Mili
+	a, b := swap("loyalist ", "college ")
+	print(a, b, "\n")
+
+	//Kartik
 	largestnumber()
+
+	//Roshni
 	vowels("E")
 	vowels("b")
+
+	//Sanjay
+	fmt.Println(calculator("add", 5, 4))
+	fmt.Println(calculator("subtract", 5, 4))
+	fmt.Println(calculator("multiply", 5, 4))
+	fmt.Println(calculator("division", 5, 4))
+	fmt.Println(calculator("modulus", 5, 3))
+
+	//Avneet
+	tablePrinting()
+
+	//Ankita
+	arithematic_opertn()
 }
--- tablePrinting.go
+++ tablePrinting.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-func tablePrinting(num int) {
+func tablePrinting() {
+	var num int
+	print("Which number of multiplication table you want: ")
+	fmt.Scan(&num)
+
 	fmt.Printf("Multiplication Table of %d:\n", num)
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("%d x %d = %d\n", num, i, num*i)
